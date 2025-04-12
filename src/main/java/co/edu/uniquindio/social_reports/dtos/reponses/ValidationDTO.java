0356a10d4b102e33d4972637b052c1dc79a57080
Perdomo Cardenas Juan Manuel <perdomocardenas18@gmail.com>
2025-04-12 16:44:19 -0500

    Se arreglo el problema de las exceptiones, ahora se pueden diferenciart

---
+++ src/main/java/co/edu/uniquindio/social_reports/dtos/reponses/ValidationDTO.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.social_reports.dtos.reponses;
+
+public record ValidationDTO(
+        String field,
+        String message
+) {
+}
---
+++ src/main/java/co/edu/uniquindio/social_reports/exceptions/handler/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package co.edu.uniquindio.social_reports.exceptions.handler;
+
+import co.edu.uniquindio.social_reports.dtos.reponses.MessageDTO;
+import co.edu.uniquindio.social_reports.dtos.reponses.ValidationDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<MessageDTO<String>> generalException(Exception e){
+    return ResponseEntity.internalServerError().body( new MessageDTO<>(true, e.getMessage()) );
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<MessageDTO<List<ValidationDTO>>> validationException(MethodArgumentNotValidException ex ) {
+    List<ValidationDTO> errors = new ArrayList<>();
+    BindingResult results = ex.getBindingResult();
+
+
+    for (FieldError e: results.getFieldErrors()) {
+      errors.add( new ValidationDTO(e.getField(), e.getDefaultMessage()) );
+    }
+
+
+    return ResponseEntity.badRequest().body( new MessageDTO<>(true, errors) );
+  }
+}
