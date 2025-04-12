305ac24eb68ad1d76acf8025fe768b5003975d62
diego3277 <diegolopez3277@gmail.com>
2025-04-12 02:33:16 -0500

    Se corriegieron las validacionesne RegisterUserDTO, se agrego la propiedad para conectarse a mongodb al archivo application.properties, los repositorios ahora extienden de MongoRepository

--- src/main/java/co/edu/uniquindio/social_reports/repositories/NotificationRepository.java
+++ src/main/java/co/edu/uniquindio/social_reports/repositories/NotificationRepository.java
@@ -1,7 +1,10 @@
 package co.edu.uniquindio.social_reports.repositories;
 
+import co.edu.uniquindio.social_reports.model.entities.Notification;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NotificationRepository {
+public interface NotificationRepository extends MongoRepository<Notification, ObjectId> {
 }
--- src/main/java/co/edu/uniquindio/social_reports/repositories/ReportRepository.java
+++ src/main/java/co/edu/uniquindio/social_reports/repositories/ReportRepository.java
@@ -1,7 +1,10 @@
 package co.edu.uniquindio.social_reports.repositories;
 
+import co.edu.uniquindio.social_reports.model.entities.Report;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReportRepository {
+public interface ReportRepository extends MongoRepository<Report, ObjectId> {
 }
