8421f866e879ceb954647f00ee22bb448b86baba
diego3277 <diegolopez3277@gmail.com>
2025-04-12 13:59:59 -0500

    Se corrigieron validaciones en DTOS de User

--- src/main/java/co/edu/uniquindio/social_reports/dtos/user/ChangePasswordDTO.java
+++ src/main/java/co/edu/uniquindio/social_reports/dtos/user/ChangePasswordDTO.java
@@ -1,13 +1,10 @@
 package co.edu.uniquindio.social_reports.dtos.user;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 
 public record ChangePasswordDTO(
         @Email String email,
         @NotBlank String recoverCode,
-        @NotBlank @Max(16) @Min(8) String newPassword
+        @NotBlank @Size(min=8, max=16) String newPassword
 ) {
 }
