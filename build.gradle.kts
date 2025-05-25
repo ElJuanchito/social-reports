70867e085d03b5667e48b98710aac2bdd7ebe57e
Perdomo Cardenas Juan Manuel <perdomocardenas18@gmail.com>
2025-05-24 21:39:28 -0500

    Agregados cosos para despliegue

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,3 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
-
-tasks.withType<Test> {
-	useJUnitPlatform()
-}
